055a38c692e20d8274d56c56a170c4bf1e2f908f
vithjook <31311666+vithjook@users.noreply.github.com>
2022-02-22 23:52:36 +0100

    Link

--- pages/index.tsx
+++ pages/index.tsx
@@ -221,7 +221,7 @@ const Home: NextPage = () => {
           </div>
         </section>
 
-        <section className="py-20 mt-20">
+      <section className="py-20 mt-20">
       
       <div className="sm:w-3/4 lg:w-5/12 mx-auto px-2">
         <h1 className="text-3xl text-center text-blue-400">Download the extension</h1>
@@ -231,57 +231,61 @@ const Home: NextPage = () => {
         </p>
       </div>
       
-      <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 max-w-screen-lg mt-16">
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 max-w-screen-lg mt-16">
+          
+          <div className="flex flex-col rounded-md shadow-md lg:mb-16">
+            <div className="p-6 flex flex-col items-center">
+              <img src="/logo-chrome.svg" alt="" />
+              <h3 className="mt-5 mb-2 text-blue-400 text-lg">Add to Chrome</h3>
+              <p className="mb-2 text-gray-400 font-light">Minimum version 62</p>
+            </div>
+            <hr className="border-b border-white" />
+            <div className="flex p-6">
+              <button type="button" className="flex-1 btn btn-purple hover:bg-white hover:text-black">
+                Add & Install Extension
+              </button>
+            </div>
+          </div>
         
-        <div className="flex flex-col rounded-md shadow-md lg:mb-16">
-          <div className="p-6 flex flex-col items-center">
-            <img src="/logo-chrome.svg" alt="" />
-            <h3 className="mt-5 mb-2 text-blue-400 text-lg">Add to Chrome</h3>
-            <p className="mb-2 text-gray-400 font-light">Minimum version 62</p>
-          </div>
-          <hr className="border-b border-white" />
-          <div className="flex p-6">
-            <button type="button" className="flex-1 btn btn-purple hover:bg-white hover:text-black">
-              Add & Install Extension
-            </button>
-          </div>
-        </div>
-       
+          
         
-      
+          
+          <div className="flex flex-col rounded-md shadow-md lg:my-8">
+            <div className="p-6 flex flex-col items-center">
+              <img src="/logo-firefox.svg" alt="" />
+              <h3 className="mt-5 mb-2 text-blue-400 text-lg">Add to Firefox</h3>
+              <p className="mb-2 text-gray-400 font-light">Minimum version 62</p>
+            </div>
+            <hr className="border-b border-white" />
+            <div className="flex p-6">
+              <button type="button" className="flex-1 btn btn-purple hover:bg-white hover:text-black">
+                Add & Install Extension
+              </button>
+            </div>
+          </div>
+          
         
-        <div className="flex flex-col rounded-md shadow-md lg:my-8">
-          <div className="p-6 flex flex-col items-center">
-            <img src="/logo-firefox.svg" alt="" />
-            <h3 className="mt-5 mb-2 text-blue-400 text-lg">Add to Firefox</h3>
-            <p className="mb-2 text-gray-400 font-light">Minimum version 62</p>
-          </div>
-          <hr className="border-b border-white" />
-          <div className="flex p-6">
-            <button type="button" className="flex-1 btn btn-purple hover:bg-white hover:text-black">
-              Add & Install Extension
-            </button>
-          </div>
-        </div>
         
-       
-      
-        
-        <div className="flex flex-col rounded-md shadow-md lg:mt-16">
-          <div className="p-6 flex flex-col items-center">
-            <img src="/logo-opera.svg" alt="" />
-            <h3 className="mt-5 mb-2 text-blue-400 text-lg">Add to Opera</h3>
-            <p className="mb-2 text-gray-400 font-light">Minimum version 62</p>
+          
+          <div className="flex flex-col rounded-md shadow-md lg:mt-16">
+            <div className="p-6 flex flex-col items-center">
+              <img src="/logo-opera.svg" alt="" />
+              <h3 className="mt-5 mb-2 text-blue-400 text-lg">Add to Opera</h3>
+              <p className="mb-2 text-gray-400 font-light">Minimum version 62</p>
+            </div>
+            <hr className="border-b border-white" />
+            <div className="flex p-6">
+              <button type="button" className="flex-1 btn btn-purple hover:bg-white hover:text-black">
+                Add & Install Extension
+              </button>
+            </div>
           </div>
-          <hr className="border-b border-white" />
-          <div className="flex p-6">
-            <button type="button" className="flex-1 btn btn-purple hover:bg-white hover:text-black">
-              Add & Install Extension
-            </button>
           </div>
-        </div>
-        </div>
 
+        </section>
+
+        <section>
+          
         </section>
 
         <main className={styles.main}>
