a63ac4731be598f30c487e1f04f8c5d3ba4d9137
vithjook <31311666+vithjook@users.noreply.github.com>
2022-02-22 20:58:28 +0100

    More_Info_Button

---
+++ components/More_Info_button.tsx
@@ -0,0 +1,5 @@
+const More_Info_button = () => (
+<button type="button" className="btn btn-purple hover:bg-white hover:text-black">More Info</button>
+)
+
+export default More_Info_button
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import PersonalSlogan from '../components/PersonalSlogan'
 import { relative } from 'path/posix'
+import More_Info_button from '../components/More_Info_button'
 
 const Home: NextPage = () => {
   return (
@@ -88,7 +89,7 @@ const Home: NextPage = () => {
       </div>
 
       <div className="relative mt-20 lg:mt-24">
-        <div className="container flex flex-col lg:flex-row items-center justify-center gap-x-24"></div>
+        <div className="container flex flex-col lg:flex-row items-center justify-center gap-x-24">
         <div className="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
             <img
               className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-max md:h-max"
@@ -104,9 +105,9 @@ const Home: NextPage = () => {
               Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control
               over how you manage your favourite sites.
             </p>
-            <button type="button" className="btn btn-purple hover:bg-white hover:text-black">More Info</button>
+            <More_Info_button/>
           </div>
-        </div>
+          </div>
         <div
           className="
             hidden
@@ -121,6 +122,93 @@ const Home: NextPage = () => {
             -left-36
           "
         ></div>
+        </div>
+       
+
+
+
+
+        
+
+
+        <div className="relative mt-20 lg:mt-52">
+        <div className="container flex flex-col lg:flex-row items-center justify-center gap-x-24">
+        <div className="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
+            <img
+              className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-max md:h-max"
+              src="/illustration-features-tab-2.png"
+              alt="illustration"
+            />
+        </div>
+      
+
+    <div className="flex flex-1 flex-col items-center lg:items-start">
+            <h1 className="text-3xl text-blue-400"> Intelligent search </h1>
+            <p className="text-gray-500 my-4 text-center lg:text-left sm:w-3/4 lg:w-full">
+              Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control
+              over how you manage your favourite sites.
+            </p>
+            <More_Info_button/>
+          </div>
+          </div>
+        <div
+          className="
+            hidden
+            lg:block
+            overflow-hidden
+            bg-purple-400
+            rounded-r-full
+            absolute
+            h-80
+            w-2/4
+            -bottom-24
+            -right-36
+          "
+        ></div>
+        </div>
+
+
+
+
+
+
+
+
+        <div className="relative mt-20 lg:mt-52">
+        <div className="container flex flex-col lg:flex-row items-center justify-center gap-x-24">
+        <div className="flex flex-1 justify-center z-10 mb-10 lg:mb-0">
+            <img
+              className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 md:w-max md:h-max"
+              src="/illustration-features-tab-3.png"
+              alt="illustration"
+            />
+        </div>
+      
+
+    <div className="flex flex-1 flex-col items-center lg:items-start">
+            <h1 className="text-3xl text-blue-400">Bookmark in one click</h1>
+            <p className="text-gray-500 my-4 text-center lg:text-left sm:w-3/4 lg:w-full">
+              Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control
+              over how you manage your favourite sites.
+            </p>
+            <More_Info_button/>
+          </div>
+          </div>
+        <div
+          className="
+            hidden
+            lg:block
+            overflow-hidden
+            bg-purple-400
+            rounded-r-full
+            absolute
+            h-80
+            w-2/4
+            -bottom-24
+            -left-36
+          "
+        ></div>
+        </div>
 
 
 
