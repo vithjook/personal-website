798dea1ca227409cdc51352001f3e78906c361b6
vithjook <31311666+vithjook@users.noreply.github.com>
2022-02-23 23:30:48 +0100

    feat: add profile picture to hero section with ping

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
+      keyframes: {
+        ping: {
+          "75%, 100%": {
+            transform: "scale(2) translate(-25%, -25%)",
+            "transform-origin": "center center",
+            opacity: 0.5,
+          },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
